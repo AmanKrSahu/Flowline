c925a55224112dd852832db26652a2d6293f05e8
Aman <amankrsahu003@gmail.com>
2024-11-16 14:27:00 +0530

    feat: Added a session middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data, isLoading, router]);
+
+  return (
+    <div>
+      Only visible to authorised users.
+      <Button onClick={() => mutate()}>Logout</Button>
+    </div>
+  );
 }
--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -8,7 +8,14 @@ import { createAdminClient } from "@/lib/appwrite";
 
 import { loginSchema, registerSchema } from "@/features/schemas";
 
+import { sessionMiddleware } from "@/lib/session-middleware";
+
 const app = new Hono()
+  .get("/current", sessionMiddleware, (c) => {
+    const user = c.get("user");
+
+    return c.json({ data: user });
+  })
   .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } = c.req.valid("json");
 
@@ -42,8 +49,11 @@ const app = new Hono()
 
     return c.json({ success: true });
   })
-  .post("/logout", (c) => {
+  .post("/logout", sessionMiddleware, async (c) => {
+    const account = c.get("account");
+
     deleteCookie(c, AUTH_COOKIE);
+    await account.deleteSession("current");
 
     return c.json({ success: true });
   });
