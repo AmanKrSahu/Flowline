51c710e99dc2a508668d11966008245a73385401
Aman <amankrsahu003@gmail.com>
2024-11-17 01:34:41 +0530

    feat: Added a workspace form

--- src/features/auth/api/use-register.ts
+++ src/features/auth/api/use-register.ts
@@ -3,9 +3,12 @@ import { InferRequestType, InferResponseType } from "hono";
 
 import { client } from "@/lib/rpc";
 import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
-type ResponseType = InferResponseType<typeof client.api.auth.register["$post"]>;
-type RequestType = InferRequestType<typeof client.api.auth.register["$post"]>;
+type ResponseType = InferResponseType<
+  (typeof client.api.auth.register)["$post"]
+>;
+type RequestType = InferRequestType<(typeof client.api.auth.register)["$post"]>;
 
 export const useRegister = () => {
   const router = useRouter();
@@ -15,11 +18,20 @@ export const useRegister = () => {
   const mutation = useMutation<ResponseType, Error, RequestType>({
     mutationFn: async ({ json }) => {
       const response = await client.api.auth.register["$post"]({ json });
+
+      if (!response.ok) {
+        throw new Error("Failed to register");
+      }
+
       return await response.json();
     },
     onSuccess: () => {
+      toast.success("Registered");
       router.refresh();
       queryClient.invalidateQueries({ queryKey: ["current"] });
+    },
+    onError: () => {
+      toast.error("Failed to register");
     },
   });
 
